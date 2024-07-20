b379951bf883ac1a4977d8f984005cad233d34f4
oxomi <osm0071@naver.com>
2024-07-21 01:08:27 +0900

    update

--- 0x04/연습문제/야매연결리스트.cpp
+++ 0x04/연습문제/야매연결리스트.cpp
@@ -1,4 +1,3 @@
-//채워보기
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -6,12 +5,22 @@ const int MX = 1000005;
 int dat[MX], pre[MX], nxt[MX];
 int unused = 1;
 
+//주의: 삽입할 위치로서의 addr은 실질적으로 삽입할 위치 양옆중 앞의 원소의 위치라고 보면 된다
 void insert(int addr, int num){
+  dat[unused] = num;
+
+  //삽입할 원소 입장에서 생각하는 부분
+  pre[unused] = addr;
+  nxt[unused] = nxt[addr];
+  //삽입할 위치의 양옆 원소 입장에서 생각하는 부분
+  nxt[addr] = unused;
+  if(nxt[addr] != -1) pre[nxt[addr]] = unused; //삽입할 위치의 다음 원소가 존재할때만 다음 원소의 이전 원소를 지정할 수 있게 함
 
 }
 
 void erase(int addr){
-
+  nxt[pre[addr]] = nxt[addr];
+  if(nxt[addr] != -1) pre[nxt[addr]] = pre[addr];
 }
 
 void traverse(){
@@ -25,7 +34,7 @@ void traverse(){
 
 void insert_test(){
   cout << "****** insert_test *****\n";
-  insert(0, 10); // 10(address=1)
+  insert(0, 10); // 10(address=1)  // 0번지는 더미 값이 있다고 가정
   traverse();
   insert(0, 30); // 30(address=2) 10
   traverse();
